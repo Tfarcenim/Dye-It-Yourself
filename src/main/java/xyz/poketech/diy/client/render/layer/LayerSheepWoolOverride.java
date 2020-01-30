f16f714b464ce83f4539d909c7e244616d3670ec
williambl <william@williambl.com>
2020-01-30 16:23:19 +0000

    Update LayerSheepWoolOverride

--- src/main/java/xyz/poketech/diy/client/render/layer/LayerSheepWoolOverride.java
+++ src/main/java/xyz/poketech/diy/client/render/layer/LayerSheepWoolOverride.java
@@ -2,6 +2,7 @@ package xyz.poketech.diy.client.render.layer;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
+import net.minecraft.client.renderer.Vector3f;
 import net.minecraft.client.renderer.entity.SheepRenderer;
 import net.minecraft.client.renderer.entity.layers.LayerRenderer;
 import net.minecraft.client.renderer.entity.model.SheepModel;
@@ -11,6 +12,8 @@ import net.minecraft.util.ResourceLocation;
 import xyz.poketech.diy.util.color.ColorUtil;
 import xyz.poketech.diy.util.color.NBTColorUtil;
 
+import java.awt.*;
+
 /**
  * Layer to override the sheep wool
  * Based on {@link net.minecraft.client.renderer.entity.layers.SheepWoolLayer}
@@ -18,34 +21,23 @@ import xyz.poketech.diy.util.color.NBTColorUtil;
 public class LayerSheepWoolOverride extends LayerRenderer<SheepEntity, SheepModel<SheepEntity>> {
 
     private static final ResourceLocation TEXTURE = new ResourceLocation("textures/entity/sheep/sheep_fur.png");
-    private final SheepRenderer sheepRenderer;
-    private final SheepWoolModel sheepModel = new SheepWoolModel();
+    private final SheepWoolModel<SheepEntity> sheepModel = new SheepWoolModel<>();
 
     public LayerSheepWoolOverride(SheepRenderer sheepRendererIn) {
         super(sheepRendererIn);
-        this.sheepRenderer = sheepRendererIn;
     }
 
     @Override
     public void render(MatrixStack matrixStackIn, IRenderTypeBuffer bufferIn, int packedLightIn, SheepEntity entitylivingbaseIn, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
-
         if (!entitylivingbaseIn.getSheared() && !entitylivingbaseIn.isInvisible()) {
-
             //Only render if a color is set
             if (entitylivingbaseIn.getPersistentData().contains(NBTColorUtil.COLOR_KEY)) {
-
-                if(entitylivingbaseIn.hasCustomName()) {
-                    if(entitylivingbaseIn.getCustomName().getFormattedText().equals("jeb_"))) {
-                        return; //Don't render on top of jeb_ sheep
-                    }
+                if(entitylivingbaseIn.hasCustomName() && "jeb_".equals(entitylivingbaseIn.getCustomName().getUnformattedComponentText())) {
+                    return; //Don't render on top of jeb_ sheep
                 }
-
-                this.sheepRenderer.bindTexture(TEXTURE);
-                ColorUtil.setGLColorFromInt(entitylivingbaseIn.getEntityData().getInteger(NBTColorUtil.COLOR_KEY));
-                this.sheepModel.setModelAttributes(this.sheepRenderer.getMainModel());
-                this.sheepModel.setLivingAnimations(entitylivingbaseIn, limbSwing, limbSwingAmount, partialTicks);
-                this.sheepModel.render(entitylivingbaseIn, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale);
             }
+            Vector3f color = ColorUtil.toFloat(entitylivingbaseIn.getPersistentData().getInt(NBTColorUtil.COLOR_KEY));
+            renderCopyCutoutModel(this.getEntityModel(), this.sheepModel, TEXTURE, matrixStackIn, bufferIn, packedLightIn, entitylivingbaseIn, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, partialTicks, color.getX(), color.getY(), color.getZ());
         }
     }
 
